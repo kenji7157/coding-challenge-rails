5ff394edf1a3d03b76656d8b2ff96fa492887524
kenji7157 <kawake.kk@gmail.com>
2023-01-22 15:53:14 +0900

    BasicChargeモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_063014) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_064343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "basic_charges", force: :cascade do |t|
+    t.bigint "plan_id", null: false
+    t.integer "ampere", null: false
+    t.decimal "charge", precision: 12, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plan_id"], name: "index_basic_charges_on_plan_id"
+  end
 
   create_table "plans", force: :cascade do |t|
     t.bigint "provider_id"
@@ -28,5 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_063014) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "basic_charges", "plans"
   add_foreign_key "plans", "providers"
 end
