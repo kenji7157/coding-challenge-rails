696a4b87c4dc2ac9a512e882b219381dee161849
kenji7157 <kawake.kk@gmail.com>
2023-01-22 15:42:07 +0900

    dbの作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_063014) do
   end
 
   create_table "providers", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
