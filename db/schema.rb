dc56f59190863006d7516b4ab6d818f40cf95979
kenji7157 <kawake.kk@gmail.com>
2023-01-22 15:41:38 +0900

    プランの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_152148) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_063014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.bigint "provider_id"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider_id"], name: "index_plans_on_provider_id"
+  end
 
   create_table "providers", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_152148) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "plans", "providers"
 end
