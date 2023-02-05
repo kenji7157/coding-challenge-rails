be1eb55a522cd95b732636d1c6e2aa464fd0fffe
kenji7157 <kawake.kk@gmail.com>
2023-02-06 07:10:58 +0900

    コメント整理

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -6,12 +6,10 @@ module Api
         kwh = params[:kwh]
         response = [] 
 
-        # TODO: preloadちゃんと調べる
-        Plan.preload(:basic_charges).find_each do |plan|
-      
+        Plan.preload(:basic_charges).find_each do |plan|      
           # 1. 基本料金を計算
           basic_charge = plan.basic_charge_by(ampere)
-          # # NOTE: ここでnextを実行することによって、該当する契約容量を持たないプランはレスポンス候補から外れる
+          # NOTE: ここでnextを実行することによって、該当する契約容量を持たないプランはレスポンス候補から外れる
           if basic_charge.blank?
             next
           end
@@ -19,12 +17,12 @@ module Api
           # 2. 従量料金を計算
           total_commodity_charge = plan.commodity_charge_by(kwh)
           
+          # 3. レスポンスのオブジェクトを生成 
           simulation_result = {
             provider_name: plan.provider.name,
             plan_name: plan.name,
             price: basic_charge + total_commodity_charge,
           }
-
           response.push(simulation_result)
         end
 
