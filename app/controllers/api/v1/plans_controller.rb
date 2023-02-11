b0589cafaaac4d5a1cbac2cffc47e16b912f547d
kenji7157 <kawake.kk@gmail.com>
2023-02-11 15:31:00 +0900

    入力チェックの修正

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -2,28 +2,27 @@ module Api
   module V1
     class PlansController < ApplicationController
       def index
-        ampere = plan_params[:ampere].to_i
-        kwh = plan_params[:kwh].to_i
-        contract_ampere_list = [10, 15, 20, 30, 40, 50, 60]
+        ampere = plan_params[:ampere]
+        kwh = plan_params[:kwh]
         errors = []
 
         # 入力チェック
-        if contract_ampere_list.exclude?(ampere)
+        if ampere.to_s !~ /10|15|20|30|40|50|60/
           errors.push("契約アンペア数は[10 / 15 / 20 / 30 / 40 / 50 / 60]のいずれかの値を入力してください")
         end
-        errors.push("使用量は0以上の整数を入力してください") if kwh <= 0
+        errors.push("使用量は0以上の整数を入力してください") if kwh.to_s !~ /^[1-9][0-9]+$/
         return render json: { errors: }, status: 400 unless errors.empty?
 
         # シミュレーション結果の取得
         response = []
         Plan.preload(:basic_charges).find_each do |plan|
           # 1. 基本料金を計算
-          basic_charge = plan.basic_charge_by(ampere)
+          basic_charge = plan.basic_charge_by(ampere.to_i)
           # NOTE: ここでnextを実行することによって、該当する契約容量を持たないプランはレスポンス候補から外れる
           next if basic_charge.blank?
 
           # 2. 従量料金を計算
-          total_commodity_charge = plan.commodity_charge_by(kwh)
+          total_commodity_charge = plan.commodity_charge_by(kwh.to_i)
 
           # 3. レスポンスのオブジェクトを生成
           simulation_result = {
